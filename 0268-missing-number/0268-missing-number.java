93ef69bf046d7aea33ceb7fefebab5639d820432
Priyanshu Sharma <143121050+PriyanshuSharma007@users.noreply.github.com>
2024-12-20 23:53:56 +0530

    0268-MISSING-NUMBER Stats: Time: 0 ms (100%), Space: 45.4 MB (30.74%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,12 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int sum=0;
-        int result =0;
+       // best approach for this is using xor 
+        int xor1=0;
+        int xor2=0;
         for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
+            xor2=xor2^nums[i];
+            xor1=xor1^(i);
         }
-        result=((nums.length*(nums.length+1))/2)-sum;
-        return result;
-        
+        xor1=xor1^(nums.length);
+        return xor1^xor2;
     }
 }
