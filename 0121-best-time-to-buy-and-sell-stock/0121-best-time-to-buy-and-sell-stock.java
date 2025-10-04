9afac74ba63acafb34556711ecb2c9e1a9bf51a5
Priyanshu Sharma <143121050+PriyanshuSharma007@users.noreply.github.com>
2025-10-04 13:38:09 +0530

    Time: 1 ms (99.82%), Space: 61.7 MB (53.39%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,17 @@
 class Solution {
-    public int maxProfit(int[] prices) { 
-        /*
-        in this question we have travel through whole array and chk 
-        if the buying price is the min before selling the stock if yes then sell it .
-         */
-        int maxprofit=0;
-        int minprice=Integer.MAX_VALUE;
+    public int maxProfit(int[] prices) {
+
+        int buyPrice=Integer.MAX_VALUE;
+        int maxprofit= 0;
+        int profit=0;
         for(int i=0;i<prices.length;i++){
-            minprice=Math.min(minprice, prices[i]);
-            maxprofit=Math.max(maxprofit, prices[i]-minprice);
+            if(buyPrice<prices[i]){
+                profit=prices[i]-buyPrice;
+                maxprofit=Math.max(profit, maxprofit);
+            } 
+            else {
+                buyPrice=prices[i];
+            }
         }
         return maxprofit;
     }
